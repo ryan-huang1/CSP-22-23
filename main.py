2f14e01fade3a3e7e99272bb6d93f9b0a064e1f8
Ryan Huang <59319094+Ryan-Huang1@users.noreply.github.com>
2022-09-16 12:23:42 -0400

    done

--- main.py
+++ main.py
@@ -5,24 +5,32 @@ import turtle as trtl
 painter = trtl.Turtle()
 
 # add code here for a circle
-
+painter.circle(50)
+painter.penup()
 
 # move the turtle to another part of the screen
-
+painter.goto(100, 100)
 
 # add code here for an arc
-
+painter.pendown()
+painter.circle(100, 180)
+painter.penup()
 
 # move the turtle to another part of the screen
-
+painter.goto(150, 150)
 
 # add code here for an arc that is greater than 90 degrees and has 5 steps
-
+painter.pendown()
+painter.circle(100, 180, 5)
+painter.penup()
 
 # move the turtle to another part of the screen
-
+painter.goto(-100, -100)
 
 # add code here to create a polygon of your choice using the circle method
+painter.pendown()
+painter.circle(50, 360, 5)
+painter.penup()
 
 # create screen object and make it persist
 wn = trtl.Screen()
