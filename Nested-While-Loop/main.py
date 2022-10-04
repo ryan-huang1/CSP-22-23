eddd380c9b17e238dd50057d4a8b5316917f08c9
Ryan Huang <59319094+Ryan-Huang1@users.noreply.github.com>
2022-10-04 12:01:04 -0400

    done

--- Nested-While-Loop/main.py
+++ Nested-While-Loop/main.py
@@ -2,10 +2,15 @@
 input = int(input("Enter a positive number between 1 and 25: "))
 
 cycles = 0
+var_one = 0
+var_two = 1
 while cycles == 0:    
-    for i in range(input+1):
-        print(i*"*")
-    for i in range(input+1):
-        print((input-i)*"*")
+    while var_one < input+1:
+        print(var_one*"*")
+        var_one += 1
+    print(input*"*")
+    while var_two < input+2:
+        print((input-var_two)*"*")
+        var_two += 1
     cycles += 1
    
