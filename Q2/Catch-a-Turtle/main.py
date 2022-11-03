fc8c400088605d2d241ee10f39a275c6a0f265e2
Ryan Huang <59319094+Ryan-Huang1@users.noreply.github.com>
2022-11-02 20:20:09 -0400

    randomly change turtle position onclick

--- Q2/Catch-a-Turtle/main.py
+++ Q2/Catch-a-Turtle/main.py
@@ -1,6 +1,7 @@
 # a121_catch_a_turtle.py
 #-----import statements-----
 import turtle as trtl
+import random as rand
 
 #-----game configuration----
 
@@ -27,25 +28,36 @@ spot_painter.shape("circle")
 spot_painter.shapesize(spot_size)
 spot_painter.fillcolor(spot_color)
 
-triangle_painter = trtl.Turtle()
-triangle_painter.shape("triangle")
-triangle_painter.shapesize(triangle_size)
-triangle_painter.fillcolor(triangle_color)
+# triangle_painter = trtl.Turtle()
+# triangle_painter.shape("triangle")
+# triangle_painter.shapesize(triangle_size)
+# triangle_painter.fillcolor(triangle_color)
 
-square_painter = trtl.Turtle()
-square_painter.shape("square")
-square_painter.shapesize(square_size)
-square_painter.fillcolor(square_color)
+# square_painter = trtl.Turtle()
+# square_painter.shape("square")
+# square_painter.shapesize(square_size)
+# square_painter.fillcolor(square_color)
 
-classic_painter = trtl.Turtle()
-classic_painter.shape("classic")
-classic_painter.shapesize(classic_size)
-classic_painter.fillcolor(classic_color)
+# classic_painter = trtl.Turtle()
+# classic_painter.shape("classic")
+# classic_painter.shapesize(classic_size)
+# classic_painter.fillcolor(classic_color)
 
 #-----game functions--------
 
+def spot_clicked(x, y):
+    print("spot clicked")
+    change_position()
+
+def change_position():
+    spot_painter.penup()
+    spot_painter.hideturtle()
+    spot_painter.goto(rand.randint(-350,350), rand.randint(-250,250))
+    spot_painter.showturtle()
+    spot_painter.pendown()
 
 #-----events----------------
 
+spot_painter.onclick(spot_clicked)
 wn = trtl.Screen()
 wn.mainloop()
