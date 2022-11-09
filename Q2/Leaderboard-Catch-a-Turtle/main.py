5a32ac5019bae32e0644895fd805833e2e02d3ca
Ryan Huang <59319094+Ryan-Huang1@users.noreply.github.com>
2022-11-09 12:16:06 -0500

    leaderboard set up

--- Q2/Leaderboard-Catch-a-Turtle/main.py
+++ Q2/Leaderboard-Catch-a-Turtle/main.py
@@ -2,6 +2,7 @@
 #-----import statements-----
 import turtle as trtl
 import random as rand
+import leaderboard as lb
 
 #-----game configuration----
 
@@ -17,6 +18,9 @@ spot_size = 2
 spot_shape= "circle"
 
 #-----initialize turtle-----
+
+leaderboard_file_name = "a122_leaderboard.txt"
+player_name = input("Please enter your name: ")
 
 spot_painter = trtl.Turtle()
 spot_painter.shape("circle")
@@ -34,6 +38,23 @@ score_writer.write(score, font=font_setup)
 
 #-----game functions--------
 
+def manage_leaderboard():
+
+  global score
+  global spot
+
+  # get the names and scores from the leaderboard file
+  leader_names_list = lb.get_names(leaderboard_file_name)
+  leader_scores_list = lb.get_scores(leaderboard_file_name)
+
+  # show the leaderboard with or without the current player
+  if (len(leader_scores_list) < 5 or score >= leader_scores_list[4]):
+    lb.update_leaderboard(leaderboard_file_name, leader_names_list, leader_scores_list, player_name, score)
+    lb.draw_leaderboard(True, leader_names_list, leader_scores_list, spot, score)
+
+  else:
+    lb.draw_leaderboard(False, leader_names_list, leader_scores_list, spot, score)
+
 def update_score():
     global score
     score += 1
@@ -47,6 +68,7 @@ def countdown():
   if timer <= 0:
     counter.write("Time's Up", font=font_setup)
     timer_up = True
+    manage_leaderboard()
   else:
     counter.write("Timer: " + str(timer), font=font_setup)
     timer -= 1
