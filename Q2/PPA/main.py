4ccbd8861d9916cf7c231fbd6360734b8caaa62f
Ryan Huang <59319094+Ryan-Huang1@users.noreply.github.com>
2022-11-17 11:39:26 -0500

    create problems 4-5

--- Q2/PPA/main.py
+++ Q2/PPA/main.py
@@ -13,3 +13,17 @@ print('\t',ppa.per(20,0),'%')
 # 3
 print('\nsum_1_100\n')
 print('\t',ppa.sum_1_100())
+
+# 4
+print('\nprnt_ints(i,j)')
+print('\t')
+ppa.prnt_ints(1,5)
+ppa.prnt_ints(-4,2)
+ppa.prnt_ints(0,6)
+
+
+# 5
+print('\nsum_ints(i,j)')
+print('\t',ppa.sum_ints(1,5))
+print('\t',ppa.sum_ints(-4,2))
+print('\t',ppa.sum_ints(0,6))
