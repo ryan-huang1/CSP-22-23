6be406a22316b8e6f8cabaebc61c8abad84fa9ba
Ryan Huang <59319094+Ryan-Huang1@users.noreply.github.com>
2023-02-12 16:32:24 -0500

    short plus long QTc finish

--- Q3/Heartbeats/main.py
+++ Q3/Heartbeats/main.py
@@ -3,11 +3,28 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-#reading the csv file
-df = pd.read_csv('Q3/Heartbeats/ecg_csv.csv')
+# reading the CSV file and creating a dataframe
+dataframe = pd.read_csv('Q3/Heartbeats/ecg_csv.csv')
+dataframe = dataframe.dropna()
 
-#converting the csv file into a dataframe
-df = pd.DataFrame(df)
+# remove rows with null or non-numeric values in QTc column
+dataframe = dataframe[pd.to_numeric(dataframe['QTc'], errors='coerce').notnull()]
 
-#print each column of the dataframe
-print(df['personid'])
+# convert QTc column to numeric data type
+dataframe['QTc'] = pd.to_numeric(dataframe['QTc'], errors='coerce')
+
+# remove rows with negative values in QTc column
+dataframe = dataframe[dataframe['QTc'] >= 0]
+print(dataframe)
+
+# select rows with QTc > 500 and get number of unique patients
+num_patients = len(dataframe[dataframe['QTc'] > 500]['personid'].unique())
+
+# print the result
+print('Number of unique patients with a QTc > 500: ', num_patients)
+
+# select rows with QTc < 350 and get number of unique patients
+num_patients = len(dataframe[dataframe['QTc'] < 350]['personid'].unique())
+
+# print the result
+print('Number of unique patients with a QTc < 350: ', num_patients)
