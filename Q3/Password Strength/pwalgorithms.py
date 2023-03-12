da5f5147e04d730ec03c3e3a9a164d13ce23d197
Ryan Huang <59319094+ryan-huang1@users.noreply.github.com>
2023-03-12 14:22:32 -0400

    base project commit

---
+++ Q3/Password Strength/pwalgorithms.py
@@ -0,0 +1,22 @@
+# Module pwalgorithms
+
+# get words from password dictionary file
+def get_dictionary():
+  words = []
+  dictionary_file = open("Q3/Password Strength/dictionary.txt")
+  for line in dictionary_file:
+    # store word, omitting trailing new-line
+    words.append(line[:-1])
+  dictionary_file.close()
+  return words
+
+# analyze a one-word password
+def one_word(password):
+  words = get_dictionary()
+  guesses = 0
+  # get each word from the dictionary file
+  for w in words:
+    guesses += 1
+    if (w == password):
+      return True, guesses
+  return False, guesses
