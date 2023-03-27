5a28ac63f64de1427d5f9b9293ba29e373493720
Ryan Huang <59319094+ryan-huang1@users.noreply.github.com>
2023-03-27 14:08:59 -0400

    password eval, finish

--- Q3/Password Strength/main.py
+++ Q3/Password Strength/main.py
@@ -24,11 +24,12 @@ print("Analyzing a two-word password ...")
 time_start = time.time()
 
 # attempt to find password
-found, num_guesses = pwa.two_words(password)
+found, num_guesses, time_taken = pwa.two_words(password)
 time_end = time.time()
 
 # report results
 if (found):
     print(password, "found in", num_guesses, "guesses")
+    print("Time:", format((time_end-time_start), ".8f"))
 else:
     print(password, "NOT found in", num_guesses, "guesses!")
--- Q3/Password Strength/pwalgorithms.py
+++ Q3/Password Strength/pwalgorithms.py
@@ -1,4 +1,5 @@
 # Module pwalgorithms
+import time
 
 # get words from password dictionary file
 def get_dictionary():
@@ -20,33 +21,24 @@ def one_word(password):
     if (w == password):
       return True, guesses
   return False, guesses
-
-# #analyze a two-word password
-# def two_words(password):
-#     words = get_dictionary()
-#     guesses = 0
-#     # get each word from the dictionary file
-#     for w1 in words:
-#         for w2 in words:
-#             guesses += 1
-#             if (w1+w2 == password):
-#                 return True, guesses
-#         return False, guesses
     
-#analyze a two-word password
 def two_words(password):
     words = get_dictionary()
+    total_words = len(words)
     guesses = 0
-   
-    for w in words:
-        subword = password[len(w):]
-        guess = w + subword
-        guesses += 1
-        if (w == password):
-            return True, guesses
-    return False, guesses
-      
-   
-    # get each word from the dictionary file
-
-two_words("hello")
+    
+    start_time = time.time()
+    
+    for i, word1 in enumerate(words):
+        for j, word2 in enumerate(words):
+            guesses += 1
+            combined_password = word1 + word2
+            
+            if combined_password == password:
+                end_time = time.time()
+                time_taken = end_time - start_time
+                return True, guesses, time_taken
+    
+    end_time = time.time()
+    time_taken = end_time - start_time
+    return False, guesses, time_taken
