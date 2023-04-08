f7f2189fd08b5dd6ee4f74ffd6848be07e388978
Ryan Huang <59319094+ryan-huang1@users.noreply.github.com>
2023-04-07 20:15:23 -0400

    finish imessage module

--- Create-Task/imessage.py
+++ Create-Task/imessage.py
@@ -1,30 +1,34 @@
 import requests
 import os
 
-# Define the URL of the LoopMessage API endpoint
-url = 'https://server.loopmessage.com/api/v1/message/send/'
+def send_loop_message(recipient_phone_number, message_text, sender_name):
+    # Define the URL of the LoopMessage API endpoint
+    url = 'https://server.loopmessage.com/api/v1/message/send/'
 
-# Define the headers for the request
-headers = {
-    'Authorization': os.environ.get('loop_auth'),
-    'Loop-Secret-Key': os.environ.get('loop_secret'),
-    'Content-Type': 'application/json'
-}
+    # Define the headers for the request
+    headers = {
+        'Authorization': os.environ.get('loop_auth'),
+        'Loop-Secret-Key': os.environ.get('loop_secret'),
+        'Content-Type': 'application/json'
+    }
 
-RECIPIENT_PHONE_NUMBER = "13365824875"
+    # Define the body parameters for the request
+    data = {
+        'recipient': recipient_phone_number,
+        'text': message_text,
+        'sender_name': sender_name
+    }
+
+    # Make the POST request to the LoopMessage API
+    response = requests.post(url, headers=headers, json=data)
+
+    # Print the response status code and JSON response
+    print(response.status_code)
+    print(response.json())
+
+# Example usage
+RECIPIENT_PHONE_NUMBER = "+13365824875"
 MESSAGE_TEXT = "Hello, World!"
 SENDER_NAME = "Ryan"
 
-# Define the body parameters for the request
-data = {
-    'recipient': '+13365824875',
-    'text': 'Hello, World!',
-    'sender_name': 'ryan@gmail.com'
-}
-
-# Make the POST request to the LoopMessage API
-response = requests.post(url, headers=headers, json=data)
-
-# Print the response status code and JSON response
-print(response.status_code)
-print(response.json())
+send_loop_message(RECIPIENT_PHONE_NUMBER, MESSAGE_TEXT, SENDER_NAME)
